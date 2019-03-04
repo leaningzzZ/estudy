dee0b94320f37819144baba27d8a172b79cbddc9
liuzhe <1040583465@qq.com>
2019-03-05 00:22:26 +0800

    .

--- src/components/student.vue
+++ src/components/student.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    {{student.assignments}}
-    <a-table :columns="columns" :dataSource="student.assignments">
-      <template slot="operation1" slot-scope="text,record,index">
+    <a-table 
+      :rowKey="assignments=>assignments.assignment_id" 
+      :columns="columns"
+      :pagination="false" 
+      :dataSource="student.assignments">
+      <template slot="operation" slot-scope="text,record,index">
         <a-upload 
             name="file" 
             :action="uploadHost" 
@@ -16,14 +19,42 @@
             <a-button  icon="download">下载</a-button>
          </a>
       </template>
-      <template slot="operation2">
-
+      <template slot="info" slot-scope="text,record,index">
+        <div v-if="record.work">
+          <div v-if="record.work.status==0">
+            {{record.work.teacher_download_time.slice(0,10)=="0000-00-00"?//判断老师是否下载作业
+            `${record.work.commit_time}提交`:
+            `${record.work.teacher_download_time}老师下载`
+            }}
+          </div>
+          <div v-else-if="record.work.status==1">
+            <p>{{`${record.work.review_time}老师提交修改意见`}}</p>
+            <a-button @click="visible=record.work">查看详情</a-button>
+          </div>
+          <div v-else>
+            <p>{{`${record.work.review_time}老师完成批改`}}</p>
+            <a-button @click="visible=record.work">查看详情</a-button>
+          </div>
+        </div>
       </template>
     </a-table>
+    <a-modal :visible="Boolean(visible)" @cancle="visible=false">
+      <h4>批改意见:</h4>
+      <p v-if="visible&&visible.teacher_review">{{visible.teacher_review}}</p>
+      <p v-if="visible&&visible.teacher_upload_name">
+        <!-- //下载老师上传的附件 -->
+        <a-button :="`${downloadHost}?id=${visible.id}&type=teacher`">附件</a-button>
+      </p>
+    </a-modal>
   </div>
 </template>
 <script>
-const Host="http://sandbox_api.estudy.chanke.xyz"
+const Host="http://sandbox_api.estudy.chanke.xyz";
+const work_status={
+  "0":"待批改",
+  "1":"需完善",
+  "2":"已完成"
+}
 import {mapState} from "vuex"
 export default {
   props: ["student"],
@@ -44,6 +75,7 @@ export default {
   },
   data() {
     return {
+        visible:false,
         uploadHost:Host+"/student/upload",//获取学生作业信息
         downloadHost:Host+"/student/download",//学生下载作业
       columns: [
@@ -70,12 +102,16 @@ export default {
         },
         {
           title: "状态",
-          scopedSlots: {
-            customRender(text,record){
-              console.log(record)
-            }
+            customRender(text,record){  //record.work.status判断作业状态
+              return record.work? work_status[record.work.status] :"未提交"
           }
-        }, 
+        },
+        {
+          title: "信息",
+            scopedSlots: {
+            customRender: "info" //渲染组件
+          }
+        } 
       ]
     };
   }
